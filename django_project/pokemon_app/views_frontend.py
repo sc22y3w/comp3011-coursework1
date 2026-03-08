859852d93cfb0a297409c498af3ffbb10e52b2a3
sc22y3w <sc22y3w@leeds.ac.uk>
2026-03-08 17:18:12 +0000

    added index page

--- django_project/pokemon_app/views_frontend.py
+++ django_project/pokemon_app/views_frontend.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 
 
+def index_page(request):
+    """Renders the index.html page."""
+    return render(request, 'index.html')
+
+
 def pokemon_page(request):
     """Renders the pokemon.html page."""
     return render(request, 'pokemon.html')
