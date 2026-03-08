db100083679d3fac2612c9720ac22dc0b28e956c
sc22y3w <sc22y3w@leeds.ac.uk>
2026-03-08 16:43:08 +0000

    first commit

---
+++ django_project/pokemon_app/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+
+class PokemonType(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+      
+class Ability(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+      
+class Pokemon(models.Model):
+    name = models.CharField(max_length=100)
+    type = models.ManyToManyField(PokemonType, related_name='pokemon_type')
+    abilities = models.ManyToManyField(Ability, related_name='pokemon_abilities')
+    against_bug = models.FloatField(default=1.0)
+    against_dark = models.FloatField(default=1.0)
+    against_dragon = models.FloatField(default=1.0)
+    against_electric = models.FloatField(default=1.0)
+    against_fairy = models.FloatField(default=1.0)
+    against_fight = models.FloatField(default=1.0)
+    against_fire = models.FloatField(default=1.0)
+    against_flying = models.FloatField(default=1.0)
+    against_ghost = models.FloatField(default=1.0)
+    against_grass = models.FloatField(default=1.0)
+    against_ground = models.FloatField(default=1.0)
+    against_ice = models.FloatField(default=1.0)
+    against_normal = models.FloatField(default=1.0)
+    against_poison = models.FloatField(default=1.0)
+    against_psychic = models.FloatField(default=1.0)
+    against_rock = models.FloatField(default=1.0)
+    against_steel = models.FloatField(default=1.0)
+    against_water = models.FloatField(default=1.0)
+    
+    hp = models.IntegerField()
+    attack = models.IntegerField()
+    defense = models.IntegerField()
+    special_attack = models.IntegerField()
+    special_defense = models.IntegerField()
+    speed = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+      
