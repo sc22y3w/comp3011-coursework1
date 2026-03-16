cb688d8374953791dd9c3d9121aa886965c44a54
Beatrice Wong <sc22y3w@leeds.ac.uk>
2026-03-16 00:24:50 +0000

    use explicit imports for better maintainability

--- django_project/pokemon_app/views.py
+++ django_project/pokemon_app/views.py
@@ -1,2 +1,21 @@
-from .views_api import *  # noqa: F401,F403
-from .views_frontend import *  # noqa: F401,F403
+from .views_api import (
+    login_api,
+    pokemon_api,
+    register_api,
+    team_analysis_api,
+    team_detail_api,
+    teams_api,
+    top_pokemon_usage_api,
+    top_type_usage_api,
+)
+from .views_frontend import (
+    analysis_page,
+    create_team_page,
+    edit_team_page,
+    global_stats_page,
+    index_page,
+    login_page,
+    pokemon_page,
+    register_page,
+    teams_page,
+)
